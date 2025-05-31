ae61162381d399401b3c87588931a2c26acdfa5f
Emilio <emiliocantil726@gmail.com>
2025-05-31 14:19:16 -0300

    liberando cors nova api

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const produtosRoute = require('./routes/produtos')
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'https://donfriobackend.onrender.com',
   credentials: true
 }));
 
